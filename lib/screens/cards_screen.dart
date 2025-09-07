e8a19a265c2a15305e06fa62a03dd6077771814e
aloyeez <prokipchukkateryna@gmail.com>
2025-09-07 17:41:44 +0200

    add cardsScreen UI with navigation to add_card_screen

--- lib/screens/cards_screen.dart
+++ lib/screens/cards_screen.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'add_card_screen.dart';
 
 class CardsScreen extends StatefulWidget {
   const CardsScreen({super.key});
 
   @override
-  CardsScreenState createState() => CardsScreenState();
+  State<CardsScreen> createState() => _CardsScreenState();
 }
 
-class CardsScreenState extends State<CardsScreen> {
+class _CardsScreenState extends State<CardsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Cards', style: TextStyle(fontSize: 24)));
+    return Scaffold(
+      backgroundColor: const Color(0xFF1b2345),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1b2345),
+        title: const Text("My cards", style: TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AddCardScreen()),
+            );
+          },
+          child: Container(
+            width: 250,
+            height: 150,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: const Center(
+              child: Icon(
+                Icons.add,
+                size: 60,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
